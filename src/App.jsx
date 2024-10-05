969934b2da5316e2ba282103a16cbb0e59ff5f49
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2024-10-05 19:03:41 -0300

    feat: mapa

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import routes from "./routes/routes";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={routes.HOME} element={<Home />} />
-      </Routes>
-    </Router>
+    <APIProvider apiKey={"AIzaSyDRvcXg0ENBOpG97u6miPvH3_2Bdl66sCk"}>
+      <Router>
+        <Routes>
+          <Route path={routes.HOME} element={<Home />} />
+        </Routes>
+      </Router>
+    </APIProvider>
   );
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,40 @@
+import { AdvancedMarker, Map, Pin } from "@vis.gl/react-google-maps";
+import { useState } from "react";
+
 const Home = () => {
+  const [marker, setMarker] = useState(null);
+
+  const handleMapClick = (e) => {
+    console.log(e.detail.latLng.lat, e.detail.latLng.lng);
+    setMarker({
+      lat: e.detail.latLng.lat,
+      lng: e.detail.latLng.lng,
+    });
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Map
+      style={{ width: `${192 * 6}px`, height: `${108 * 6}px` }}
+      defaultCenter={{ lat: -24.029286, lng: -52.3370791 }}
+      streetView={true}
+      defaultZoom={17}
+      gestureHandling={"greedy"}
+      disableDefaultUI={true}
+      mapTypeId={"satellite"}
+      onClick={handleMapClick}
+      mapId={"f1b7b3b3b1b3b1b3"}
+      draggingCursor={""}
+    >
+      {marker && (
+        <AdvancedMarker position={{ lat: marker.lat, lng: marker.lng }}>
+          <Pin
+            background={"#0f9d58"}
+            borderColor={"#006425"}
+            glyphColor={"#60d98f"}
+          />
+        </AdvancedMarker>
+      )}
+    </Map>
   );
 };
 
