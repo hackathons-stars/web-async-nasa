f376225fe33e7409f680a9f3ef33d9c5c45e0458
Isis Lopes <lopesi@alunos.utfpr.edu.br>
2024-10-05 17:43:16 -0300

    pra ter um comecinho ne

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer style={{ textAlign: 'center', padding: '20px' }}>
+        <p>&copy; 2024 Minha Aplicação Meteorológica</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
