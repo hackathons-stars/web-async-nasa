84cbe1eb2601d36615a1fa534818a553c4dcd606
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2024-10-06 11:08:49 -0300

    dasd

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,12 +3,11 @@ import StructSlideBottom from "../../components/slide-bottom/slide-bottom";
 import ElementMap from "../../components/map/map";
 import ElementTodayWeather from "../../components/today-weather/today-weather";
 import ElementForecastWeather from "../../components/forecast-weather/forecast-weather";
-import { useState, useEffect, useRef } from 'react';
-import StructSlideBottom from '../../components/slide-bottom/slide-bottom';
-import ElementMap from '../../components/map/map';
-import ElementTodayWeather from '../../components/today-weather/today-weather';
-import ElementForecastWeather from '../../components/forecast-weather/forecast-weather';
-import { getIcon, getWeather } from "../../service/open-weather-api/open-weather-api";
+import { useState, useEffect } from "react";
+import {
+  getIcon,
+  getWeather,
+} from "../../service/open-weather-api/open-weather-api";
 import dataOpen from "./data.json";
 
 export default function Home() {
@@ -20,21 +19,21 @@ export default function Home() {
     humidity: "...",
     precipitation: "...",
     icon: "",
-    uiv: "..."
-  })
+    uiv: "...",
+  });
 
   useEffect(() => {
     const data = dataOpen;
     const { current, daily } = data;
     todayWeather.city = "Campo Mourão";
-    todayWeather.temp = current.temp
+    todayWeather.temp = current.temp;
     todayWeather.tempMax = daily.at(0).temp.max;
     todayWeather.tempMin = daily.at(0).temp.max;
     todayWeather.humidity = current.humidity;
     todayWeather.uiv = current.uvi;
     todayWeather.icon = getIcon(current.weather.at(0).icon);
     todayWeather.precipitation = "";
-    console.log({...todayWeather});
+    console.log({ ...todayWeather });
 
     /*
     console.log(data);
@@ -65,7 +64,7 @@ export default function Home() {
 
   return (
     <div id="Home">
-      <div className='containerHome'>
+      <div className="containerHome">
         <ElementTodayWeather
           city={todayWeather.city}
           tempMax={todayWeather.tempMax}
@@ -73,7 +72,9 @@ export default function Home() {
           humidity={todayWeather.humidity}
           icon={todayWeather.icon}
           uiv={todayWeather.uiv}
-          onSearch={() => { console.log("Pesquisar") }}
+          onSearch={() => {
+            console.log("Pesquisar");
+          }}
         />
         <ElementForecastWeather
           title="Previsão do tempo 8 dias"
@@ -83,7 +84,8 @@ export default function Home() {
             { temp: 10, humidity: 10, precipitation: 10 },
             { temp: 10, humidity: 10, precipitation: 10 },
             { temp: 10, humidity: 10, precipitation: 10 },
-            { temp: 10, humidity: 10, precipitation: 10 }]}
+            { temp: 10, humidity: 10, precipitation: 10 },
+          ]}
         />
         <StructSlideBottom>
           <ElementMap />
