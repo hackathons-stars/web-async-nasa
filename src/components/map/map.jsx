60424a901aa025031bb17884031d4c12cc84e131
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2024-10-06 01:29:24 -0300

    asfazzaggaagga

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,41 +1,127 @@
 import "./map.scss";
 import { AdvancedMarker, Map, Pin } from "@vis.gl/react-google-maps";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { FaPlus } from "react-icons/fa";
 
 export default function ElementMap() {
-  const [marker, setMarker] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [newLocationName, setNewLocationName] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const savedMarkers = JSON.parse(localStorage.getItem("markers")) || [];
+    setMarkers(savedMarkers);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("markers", JSON.stringify(markers));
+  }, [markers]);
 
   const handleMapClick = (e) => {
-    console.log(e.detail.latLng.lat, e.detail.latLng.lng);
-    setMarker({
+    const newMarker = {
       lat: e.detail.latLng.lat,
       lng: e.detail.latLng.lng,
-    });
+      name: "",
+    };
+    setSelectedMarker(newMarker);
   };
 
-  return (<div>
-    <Map
-      style={{ width: `${430}px`, height: `${855}px` }}
-      defaultCenter={{ lat: -24.029286, lng: -52.3370791 }}
-      streetView={true}
-      defaultZoom={17}
-      gestureHandling={"greedy"}
-      disableDefaultUI={true}
-      mapTypeId={"hybrid"}
-      onClick={handleMapClick}
-      mapId={"f1b7b3b3b1b3b1b3"}
-      draggingCursor={"move"}
-      draggableCursor={"pointer"}
-    >
-      {marker && (
-        <AdvancedMarker position={{ lat: marker.lat, lng: marker.lng }}>
-          <Pin
-            background={"#0f9d58"}
-            borderColor={"#006425"}
-            glyphColor={"#60d98f"}
-          />
-        </AdvancedMarker>
+  const handleSaveMarker = () => {
+    if (!newLocationName) return;
+
+    const newMarker = {
+      ...selectedMarker,
+      name: newLocationName,
+    };
+
+    setMarkers([...markers, newMarker]);
+    setSelectedMarker(null);
+    setNewLocationName("");
+  };
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarker(marker);
+    setShowModal(true);
+  };
+
+  return (
+    <div className="map-container">
+      <button onClick={handleSaveMarker} className="addButton">
+        <FaPlus />
+      </button>
+
+      <Map
+        style={{ width: `100%`, height: `-webkit-fill-available` }}
+        defaultCenter={{ lat: -24.029286, lng: -52.3370791 }}
+        defaultZoom={17}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+        mapTypeId={"hybrid"}
+        onClick={handleMapClick}
+        mapId={"f1b7b3b3b1b3b1b3"}
+        draggingCursor={"move"}
+        draggableCursor={"pointer"}
+      >
+        {markers.map((marker, index) => (
+          <AdvancedMarker
+            key={index}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => handleMarkerClick(marker)}
+          >
+            <Pin
+              background={"#9d0f0f"}
+              borderColor={"#640000"}
+              glyphColor={"#d96060"}
+            >
+              <div className="markerInfos">
+                <h5>{marker.name || "Posição customizada"}</h5>
+                <div className="divider"></div>
+                <div>
+                  <p>Latitude: {parseFloat(marker.lat).toFixed(6)}</p>
+                  <p>Longitude: {parseFloat(marker.lng).toFixed(6)}</p>
+                </div>
+              </div>
+            </Pin>
+          </AdvancedMarker>
+        ))}
+
+        {selectedMarker && (
+          <AdvancedMarker
+            position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
+          >
+            <Pin
+              background={"#9d0f0f"}
+              borderColor={"#640000"}
+              glyphColor={"#d96060"}
+            >
+              <div className="markerInfos">
+                <h5>Posição customizada</h5>
+                <div className="divider"></div>
+                <div>
+                  <p>
+                    <b>Latitude:</b> {parseFloat(selectedMarker.lat).toFixed(6)}
+                  </p>
+                  <p>
+                    <b>Longitude:</b>{" "}
+                    {parseFloat(selectedMarker.lng).toFixed(6)}
+                  </p>
+                </div>
+              </div>
+            </Pin>
+          </AdvancedMarker>
+        )}
+      </Map>
+
+      {showModal && selectedMarker && (
+        <div className="modal">
+          <h2>{selectedMarker.name}</h2>
+          <p>Latitude: {selectedMarker.lat}</p>
+          <p>Longitude: {selectedMarker.lng}</p>
+          <button onClick={() => setShowModal(false)}>Fechar</button>
+        </div>
       )}
-    </Map>
-  </div>);
-} 
+    </div>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,37 @@
 import "./home.scss";
-import React from 'react';
-import StructSlideBottom from '../../components/slide-bottom/slide-bottom';
-import ElementMap from '../../components/map/map';
-import ElementTodayWeather from '../../components/today-weather/today-weather';
-import ElementForecastWeather from '../../components/forecast-weather/forecast-weather';
+import StructSlideBottom from "../../components/slide-bottom/slide-bottom";
+import ElementMap from "../../components/map/map";
+import ElementTodayWeather from "../../components/today-weather/today-weather";
+import ElementForecastWeather from "../../components/forecast-weather/forecast-weather";
 
 export default function Home() {
   return (
     <div id="Home">
-      <div className='containerHome'>
-      <ElementTodayWeather
-        city="Campo Mourão"
-        tempMax="28 °C"
-        tempMin="20 °C"
-        humidity="20%"
-        precipitation="1mm"
-        onSearch={() => { console.log("Pesquisar") }}
-      />
-      <ElementForecastWeather
-        title="Previsão do tempo 8 dias"
-        date="05/10/2024"
-        data={[
-          { temp: 10, humidity: 10, precipitation: 10 },
-          { temp: 10, humidity: 10, precipitation: 10 },
-          { temp: 10, humidity: 10, precipitation: 10 },
-          { temp: 10, humidity: 10, precipitation: 10 }]}
-      />
-      <StructSlideBottom>
-        <ElementMap />
-      </StructSlideBottom>
+      <div className="containerHome">
+        <ElementTodayWeather
+          city="Campo Mourão"
+          tempMax="28 °C"
+          tempMin="20 °C"
+          humidity="20%"
+          precipitation="1mm"
+          onSearch={() => {
+            console.log("Pesquisar");
+          }}
+        />
+        <ElementForecastWeather
+          title="Previsão do tempo 8 dias"
+          date="05/10/2024"
+          data={[
+            { temp: 10, humidity: 10, precipitation: 10 },
+            { temp: 10, humidity: 10, precipitation: 10 },
+            { temp: 10, humidity: 10, precipitation: 10 },
+            { temp: 10, humidity: 10, precipitation: 10 },
+          ]}
+        />
+        <StructSlideBottom>
+          <ElementMap />
+        </StructSlideBottom>
       </div>
     </div>
   );
-};
+}
